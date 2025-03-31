17aa59f92aed9777bd2c38d20e3c81b5a7d80449
Biscoito145 <peixesapeca145@gmail.com>
2025-03-31 13:32:56 -0300

    Alteração Debug=False

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pageone import app, database
 with app.app_context():
     database.create_all()
 
-import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False)
