0f6815e742762d0faa582fd51c38e79307e5ab03
Biscoito145 <peixesapeca145@gmail.com>
2025-03-31 17:21:33 -0300

    Alteração Banco de Dados

--- pageone/__init__.py
+++ pageone/__init__.py
@@ -4,11 +4,13 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '23776f32948cb12d7cd472c5f9f52ba8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
